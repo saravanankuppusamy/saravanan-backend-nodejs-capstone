e73075b2aaba61fde62f851448252b71e0981600
Saravanan <saravanan_kuppusamy@yahoo.com>
2026-01-16 15:37:26 -0500

    Implemented API endpoints

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -57,4 +57,49 @@ router.post('/register', async (req, res) => {
     }
 });
 
+//Login Endpoint
+router.post('/login', async (req, res) => {
+    console.log("\n\n Inside login")
+
+    try {
+        //Task1 connect to `secondChance` in MongoDB through `connectToDatabase`
+        const db = await connectToDatabase();
+        //Task2 Access MongoDB `users` collection
+        const collection = db.collection("users");
+        //Task3 Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+        //Task4 Check if the password matches
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            //send appropriate message if mismatch
+            if(!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong pasword' });
+            }
+            
+            //Task5 Fetch user details
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+
+            //Task6 Create JWT authentication if passwords match
+            let payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+
+            logger.info('User logged in successfully');
+            return res.status(200).json({ authtoken, userName, userEmail });
+        //Task7 Send appropriate message if user not found
+        } else {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).json({ error: 'Internal server error', details: e.message });
+      }
+});
+
 module.exports = router;
